6d8ab82c225bba7e12a13cd2ac9c7a0354f923d1
ravi <ravi@gmail.com>
2021-12-26 22:25:13 +0530

    creating an employeepayroll app using springboot

---
+++ src/main/java/com/example/demo/EmployeePayroll1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayroll1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayroll1Application.class, args);
+	}
+
+}
